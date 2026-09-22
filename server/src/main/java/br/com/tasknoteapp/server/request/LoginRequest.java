bb414614eee1a96baa4df842d784c263e8e732f8
Ricardo Campos <ricardompcampos@gmail.com>
2026-09-22 23:48:38 +0200

    chore: redact all password records

--- server/src/main/java/br/com/tasknoteapp/server/request/LoginRequest.java
+++ server/src/main/java/br/com/tasknoteapp/server/request/LoginRequest.java
@@ -1,4 +1,15 @@
 package br.com.tasknoteapp.server.request;
 
 /** This class represents a login request with user email and password. */
-public record LoginRequest(String email, String password, String passwordAgain, String lang) {}
+public record LoginRequest(String email, String password, String passwordAgain, String lang) {
+
+  @Override
+  public String toString() {
+    return "LoginRequest{"
+        + "email=' + email + '"
+        + ",password='[REDACTED]'"
+        + ",passwordAgain='[REDACTED]'"
+        + ",lang=' + lang + '"
+        + "}";
+  }
+}
--- server/src/main/java/br/com/tasknoteapp/server/request/PasswordResetRequest.java
+++ server/src/main/java/br/com/tasknoteapp/server/request/PasswordResetRequest.java
@@ -1,4 +1,14 @@
 package br.com.tasknoteapp.server.request;
 
 /** This record represents the confirmation of the password reset. */
-public record PasswordResetRequest(String token, String password, String passwordAgain) {}
+public record PasswordResetRequest(String token, String password, String passwordAgain) {
+
+  @Override
+  public String toString() {
+    return "PasswordResetRequest{"
+        + "token=' + token + '"
+        + ",password='[REDACTED]'"
+        + ",passwordAgain='[REDACTED]'"
+        + "}";
+  }
+}
